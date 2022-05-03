853a520b04e0b148d041575cd4acb5d30b0f454f
ColTTz <103967941+ColTTz@users.noreply.github.com>
2022-05-02 21:36:35 -0300

    Add files via upload

---
+++ sd/animacao.js
@@ -0,0 +1,75 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    
+    
+    
+    for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+    
+    
+    
+    if (elementTop < windowHeight - elementVisible) {
+    reveals[i].classList.add("active");
+    } else {
+    reveals[i].classList.remove("active");
+    }
+    }
+    }
+    
+    
+    
+    window.addEventListener("scroll", reveal);
+
+
+
+    function reveal1() {
+        var reveals = document.querySelectorAll(".reveal1");
+        
+        
+        
+        for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 350;
+        
+        
+        
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        } else {
+        reveals[i].classList.remove("active");
+        }
+        }
+        }
+        
+        
+        
+        window.addEventListener("scroll", reveal1);
+
+
+
+        function reveal2() {
+            var reveals = document.querySelectorAll(".reveal2");
+            
+            
+            
+            for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 950;
+            
+            
+            
+            if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+            } else {
+            reveals[i].classList.remove("active");
+            }
+            }
+            }
+            
+            
+            
+            window.addEventListener("scroll", reveal2);
